e5e951c6f149a485e370565a99d2205438998535
pdendy <PDENDY@Skillstorm.com>
2022-11-21 15:20:41 -0500

    "update routes"

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:world_time_app/pages/choose_location.dart';
 import 'package:world_time_app/pages/home.dart';
+import 'package:world_time_app/pages/loading.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    home: Home(),
+void main() => runApp(MaterialApp(
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    },
   ));
-}
+
 
 
