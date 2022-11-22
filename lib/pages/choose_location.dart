a5a0db560f7915bae5d054515de595c88a7bdc68
pdendy <PDENDY@Skillstorm.com>
2022-11-22 15:56:29 -0500

    "update initial route, import dart:convert, update getData function"

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -9,22 +9,6 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  void getData() async {
-  //simulate network request for username
-    String username = await Future.delayed(Duration(seconds: 3), () {
-      return 'yoshi';
-    });
-
-  //simulate network request to get bio of username
-    String bio = await Future.delayed(Duration(seconds: 2), () {
-    return 'vegan, musician & egg collector';
-    });
-  }
-  @override
-  void initState() {
-    getData();
-    super.initState();
-  }
   
   @override
   Widget build(BuildContext context) {
