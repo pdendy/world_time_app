44bc4c4f95e1371dc9dd32e9fa6903dc2330b2ad
pdendy <PDENDY@Skillstorm.com>
2022-11-28 09:39:35 -0500

    "add getTimen function"

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -12,16 +12,26 @@ class Loading extends StatefulWidget {
 class _LoadingState extends State<Loading> {
 
 
-  void getData() async {
+  void getTime() async {
 
-    Response response = await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/Europe/London'));
     Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
+    //print(data);
+
+    //get properties from data
+    String dateTime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    //print(dateTime);
+    print(offset);
+
+    //create DateTime object
+    DateTime now = DateTime.parse(dateTime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
   }
   @override
   void initState() {
-    getData();
+    getTime();
     super.initState();
   }
 
